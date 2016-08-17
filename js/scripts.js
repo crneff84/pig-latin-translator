6e0dbb1cce89b8cbd07e67ad3a900606d36c7d2f
Chance Neff and Martin Cartledge <chance@>
2016-08-17 15:42:27 -0700

    update backend logic to recognize first char of string and add ay if it is a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Back-End
 
 
-
+var vowel = function(userInput) {
+  if ((userInput.charAt(0) === "a") || (userInput.charAt(0) === "A") || (userInput.charAt(0) === "e") || (userInput.charAt(0) === "E") || (userInput.charAt(0) === "i")) {
+    return userInput + "ay";
+  } else if ((userInput.charAt(0) === "I") || (userInput.charAt(0) === "o") || (userInput.charAt(0) === "O") || (userInput.charAt(0) === "u") || (userInput.charAt(0) === "U")) {
+    return userInput + "ay";
+  }  else {
+    return false;
+  };
+};
 
 
 
@@ -10,8 +18,11 @@ $(document).ready(function(){
   $("#blanks form").submit(function(event){
   event.preventDefault();
 
-  var userInput = $("#user-input").val();
-  
+  var userInput = $("input#user-input").val();
+  var result = vowel(userInput)
+
+  $("#output").text(result);
   $("#results").show();
+
   });
 });
